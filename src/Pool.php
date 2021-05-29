4afb48f2c755e17868811fcfcde5a49fa0aac399
MGatner <mgatner@icloud.com>
2021-05-29 19:40:31 +0000

    Use config default TTL

--- src/Pool.php
+++ src/Pool.php
@@ -236,7 +236,14 @@ final class Pool implements CacheItemPoolInterface
 		}
 
 		// Deteremine TTL
-		$ttl = ($expiration = $item->getExpiration()) ? Time::now()->difference($expiration)->getSeconds() : 60;
+		if ($expiration = $item->getExpiration())
+		{
+			$ttl = Time::now()->difference($expiration)->getSeconds();
+		}
+		else
+		{
+			$ttl = config('Cache')->ttl ?? 60;
+		}
 
 		return $this->adapter->save($item->getKey(), $item->get(), $ttl);
 	}
